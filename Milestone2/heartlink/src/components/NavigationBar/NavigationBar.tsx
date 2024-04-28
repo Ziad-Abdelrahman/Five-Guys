05abbbb8c0dcf0347ea2acc58acbcefba18627dd
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-04-28 16:45:45 +0300

    Adedd Dashboard and done with integration

--- Milestone2/heartlink/src/components/NavigationBar/NavigationBar.tsx
+++ Milestone2/heartlink/src/components/NavigationBar/NavigationBar.tsx
@@ -1,26 +1,21 @@
-import img from '../../assets/logo.png';
+import img from "../../assets/logo.png";
 import "./NavigationBar.css";
 
-
 interface NavigationBarProps {
-    leftDiv?: React.ReactNode;
-    rightDiv?: React.ReactNode;
+  leftDiv?: React.ReactNode;
+  rightDiv?: React.ReactNode;
 }
 
-function NavigationBar({leftDiv, rightDiv}: NavigationBarProps){
-    return(
-        <nav className="navigation-bar">
-            <div className="leftDiv">
-                {leftDiv}
-            </div>
-            <div className="middleDiv">
-                <img src={img} draggable="false"  className="logo"/>
-            </div>
-            <div className="rightDiv">
-                 {rightDiv}
-            </div>
-        </nav>
-    )
+function NavigationBar({ leftDiv, rightDiv }: NavigationBarProps) {
+  return (
+    <nav className="navigation-bar">
+      <div className="leftDiv">{leftDiv}</div>
+      <div className="middleDiv">
+        <img src={img} draggable="false" className="logo" alt={"icon"} />
+      </div>
+      <div className="rightDiv">{rightDiv}</div>
+    </nav>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
