d1910cedd35975ce0f011be7d23f4f3b641e6972
m-halawa <menna.halawa@gmail.com>
2024-05-01 01:35:58 +0300

    Update App.tsx

--- Milestone2/heartlink/src/App.tsx
+++ Milestone2/heartlink/src/App.tsx
@@ -1,7 +1,34 @@
-import CreatePost2 from "./createPost/CreatePost2.tsx";
+import React from 'react';
+import Donors from "./Admin/Donors/Donors.tsx";
+import Requests from "./Admin/Requests/Requests.tsx";
+import Organizations from "./Admin/Organizations/Organizations.tsx";
+import Navbar from "./components/Admin navbar/Navbar";
+import ChangePass from "./Admin/change password/ChangePass.tsx";
 
 function App() {
-  return <CreatePost2 />;
+  let Component;
+  switch(window.location.pathname) {
+    case "/Donors":
+      Component = Donors;
+      break;
+    case "/Organizations":
+      Component = Organizations;
+      break;
+    case "/Requests":
+      Component = Requests; 
+      break;
+    case "/changePassword":
+      Component = ChangePass; 
+      break;  
+      
+  }
+
+  return (
+    <div>
+      <Navbar />
+      {Component ? <Component /> : <div />}
+    </div>
+  );
 }
 
 export default App;
