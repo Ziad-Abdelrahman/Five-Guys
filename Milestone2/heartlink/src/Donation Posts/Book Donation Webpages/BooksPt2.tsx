9ddd982a946a4a0af3abd0b0178c7d102d379a60
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-02 10:05:59 +0300

    move(Books): moved books webpages to a separate file

--- Milestone2/heartlink/src/Donation Posts/Book Donation Webpages/BooksPt2.tsx
+++ Milestone2/heartlink/src/Donation Posts/Book Donation Webpages/BooksPt2.tsx
@@ -1,4 +1,4 @@
-import ImageUploader from "../components/FileUploader/ImageUploader.tsx";
+import ImageUploader from "../../components/FileUploader/ImageUploader.tsx";
 
 function BooksPt2() {
   let shift = { top: "10em" };
