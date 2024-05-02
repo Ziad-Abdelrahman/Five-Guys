08f6541f7642c98b9bfb255208b51fdfd45f422a
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-03 02:34:24 +0300

    refactor(App): added NavigationBar

--- Milestone2/heartlink/src/App.tsx
+++ Milestone2/heartlink/src/App.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
 import Donors from "./Admin/Donors/Donors.tsx";
 import Requests from "./Admin/Requests/Requests.tsx";
 import Organizations from "./Admin/Organizations/Organizations.tsx";
 import Navbar from "./components/Admin navbar/Navbar";
 import ChangePass from "./Admin/change password/ChangePass.tsx";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import "./Admin/admin.css";
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-    <div className='Container'>
-      
-      <Routes>
-        <Route path ="/Donors" element={<Donors/>}/>
-        <Route path ="/Organizations" element={<Organizations/>}/>
-        <Route path ="/Requests" element={<Requests/>}/>
-        <Route path ="/changePassword" element={<ChangePass/>}/>
-
-      </Routes>
-    </div>
+      <Navbar></Navbar>
+      <div className="container">
+        <Routes>
+          <Route path="/Donors" element={<Donors />} />
+          <Route path="/Organizations" element={<Organizations />} />
+          <Route path="/Requests" element={<Requests />} />
+          <Route path="/changePassword" element={<ChangePass />} />
+        </Routes>
+      </div>
     </>
-    
   );
 }
-
 
 export default App;
