2a9e03c81897f86a9b38821c73f5c906bb2102c6
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-05-03 15:37:07 +0300

    fix(Filter): fix filter button names

--- Milestone2/heartlink/src/main.tsx
+++ Milestone2/heartlink/src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Search from "./components/Search/Search.tsx";
 import AllDonationRequests from "./Donation Requests Page/AllDonationRequests.tsx";
 import ClothesRequests from "./Donation Requests Page/ClothesRequests.tsx";
+import SchoolSuppliesRequests from "./Donation Requests Page/SchoolSuppliesRequests.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClothesRequests />
+    <SchoolSuppliesRequests />
   </React.StrictMode>,
 );
