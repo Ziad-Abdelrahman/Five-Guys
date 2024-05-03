b2cb4a3f9c1c06ebf4d8e35e20ccbd4f6a1c9da7
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-03 18:19:18 +0300

    feat(Filter): add filter in food donation

--- Milestone2/heartlink/src/Donation Requests Page/FoodRequests.tsx
+++ Milestone2/heartlink/src/Donation Requests Page/FoodRequests.tsx
@@ -1,15 +1,25 @@
 import "./AllDonationRequests.css";
 import DonorNavigationBar from "../components/NavigationBar/Donor Navigation Bar/DonorNavigationBar.tsx";
-import Headerofsection from "../components/header/headerofsection.tsx";
+import Headerofsection from "../components/header/HeaderOfSection.tsx";
 import FoodBank from "../assets/EgyptianFoodBank.png";
 import Misrelkheir from "../assets/Misrelkheir.jpeg";
 import Turin from "../assets/Turin.jpg";
 import DonationRequestCard from "../components/Card/DonationRequestCard.tsx";
+import filtericon from "../assets/filter-icon.png";
+import FoodDonationFilter from "../components/DonorFilterCard/FoodDonationFilter.tsx";
 function FoodRequests() {
   return (
     <>
       <DonorNavigationBar />
       <Headerofsection title="Food Requests" />
+      <div className="filters-container">
+        <div className="filters-label">
+          <img src={filtericon} alt="Filter Icon" className="filter-icon" />
+          <strong>Filters</strong>
+        </div>
+        <FoodDonationFilter />
+      </div>
+
       <div className="requests-conainer">
         <DonationRequestCard
           image={Turin}
