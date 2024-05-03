2df4a9f239ef49f98e053a76eee1a130a4fb61b7
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-03 17:54:37 +0300

    feat(Filter): add filter in donation posts

--- Milestone2/heartlink/src/Donation Posts/View Donation Posts/ViewDonationPostsWebpage.tsx
+++ Milestone2/heartlink/src/Donation Posts/View Donation Posts/ViewDonationPostsWebpage.tsx
@@ -1,6 +1,9 @@
 import NavigationBar from "../../components/NavigationBar/NavigationBar.tsx";
-import HeaderOfSection from "../../components/header/headerofsection.tsx";
+import HeaderOfSection from "../../components/header/HeaderOfSection.tsx";
 import DonationPostCard from "../../components/Card/Organization Cards/DonationPostCard.tsx";
+import SchoolSuppliesFilter from "../../components/DonorFilterCard/SchoolSuppliesFilter.tsx";
+import filtericon from "../../assets/filter-icon.png";
+import "../../Donation Requests Page/AllDonationRequests.css";
 
 function ViewDonationPostsWebpage() {
   return (
@@ -9,6 +12,22 @@ function ViewDonationPostsWebpage() {
     >
       <NavigationBar />
       <HeaderOfSection title={"Donation Posts"} style={{ width: "1050px" }} />
+      <div
+        className="filters-container"
+        style={{ marginLeft: "14.5em", marginBottom: "10px" }}
+      >
+        <div className="filters-label">
+          <img src={filtericon} alt="Filter Icon" className="filter-icon" />
+          <strong>Filters</strong>
+        </div>
+
+        {/*filter component*/}
+        <SchoolSuppliesFilter
+          buttonName1={"All"}
+          buttonName2={"Stationary"}
+          buttonName3={"Books"}
+        />
+      </div>
       <DonationPostCard typeOfRequest={"Clothes"} />
     </div>
   );
