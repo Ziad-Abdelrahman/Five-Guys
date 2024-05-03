6ab5ed640d996aa98636a68ef285c5429b7afd11
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-03 15:34:43 +0300

    added Search

--- Milestone2/heartlink/src/components/Search/Search.tsx
+++ Milestone2/heartlink/src/components/Search/Search.tsx
@@ -1,21 +1,21 @@
 import { BsSearchHeart } from "react-icons/bs";
 
 interface SearchProps {
-    handleSearch: (written: string) => void;
+  handleSearch: (written: string) => void;
 }
 
 function Search({ handleSearch }: SearchProps) {
-    return (
-        <div className="searchBoxContent">
-            <BsSearchHeart className="searchIcon" />
+  return (
+    <div className="searchBoxContent">
+      <BsSearchHeart className="searchIcon" />
 
-            <div className="input">
-                <input
-                    placeholder="Type to search..."
-                    onChange={(e) => handleSearch(e.target.value)}
-                />
-            </div>
-        </div>
-    );
+      <div className="input">
+        <input
+          placeholder="Type to search..."
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 export default Search;
