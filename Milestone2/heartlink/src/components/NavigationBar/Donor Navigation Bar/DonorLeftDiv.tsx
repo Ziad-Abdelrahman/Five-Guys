b2400588dee18880395c38377a6cf13f7d99d4d5
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-05-04 17:59:12 +0300

    refactor(NavigationBar): add more options in dropdown

--- Milestone2/heartlink/src/components/NavigationBar/Donor Navigation Bar/DonorLeftDiv.tsx
+++ Milestone2/heartlink/src/components/NavigationBar/Donor Navigation Bar/DonorLeftDiv.tsx
@@ -1,27 +1,23 @@
-import './DonorLeftDiv.css'
-import '../../Buttons/Buttons.css'
+import "./DonorLeftDiv.css";
+import "../../Buttons/Buttons.css";
 import arrow from "../../../assets/right-arrow.png";
-function DonorLeftDiv(){
-
-    return(
-        <>
-            <div className="dropdown">
-                <button className="dropbtn">Donations
-                    <img className="arrow-in-button" src={arrow} alt="arrow"/>
-                </button>
-                <div className="dropdown-content">
-                <a href="#">View All Donations</a>
-                    <a href="#">View Categories</a>
-                    <a href="#">View posts</a>
-                </div>
-            </div>
-
-
-
-
-        </>
-    );
-
+function DonorLeftDiv() {
+  return (
+    <>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Donations
+          <img className="arrow-in-button" src={arrow} alt="arrow" />
+        </button>
+        <div className="dropdown-content">
+          <a href="#">View All Donations</a>
+          <a href="#">View Categories</a>
+          <a href="#">Teaching posts</a>
+          <a href="#">Medical posts</a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DonorLeftDiv;
