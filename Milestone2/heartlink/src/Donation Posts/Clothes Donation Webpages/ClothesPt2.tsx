040148bed5745af6645a1259d9b29d2090d306b6
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-05 04:57:39 +0300

    finished clothes donation webpages

--- Milestone2/heartlink/src/Donation Posts/Clothes Donation Webpages/ClothesPt2.tsx
+++ Milestone2/heartlink/src/Donation Posts/Clothes Donation Webpages/ClothesPt2.tsx
@@ -1,23 +1,36 @@
 import NumberInputBox from "../../components/InputBox/NumberInputBox.tsx";
 import RadioButton from "../../components/RadioButton/RadioButton.tsx";
+import Template1 from "../../components/Templates/Template1.tsx";
 function ClothesPt2() {
   return (
-    <div className="bloodDonation-container">
-      <div className="header-container">
-        <h1>Clothes</h1>
-      </div>
-      <div className="rest-container" style={{ top: "16em" }}>
-        <h4>What size of clothing are you looking for?</h4>
-        <form className="radioButtons-form" method="get">
-          <RadioButton text={"Small"} />
-          <RadioButton text={"Medium"} />
-          <RadioButton text={"Large"} />
-        </form>
-        <p />
-        <h4> How many pieces do you need?</h4>
-        <NumberInputBox label={"Quantity"} width={"280px"} />
-      </div>
-    </div>
+    <Template1
+      leftPanelDiv={
+        <div className="leftText">
+          <p /> 2 of 2<h2> Creating your post!</h2>
+          <p> Almost Done.. </p>
+        </div>
+      }
+      rightPanelDiv={
+        <div className="bloodDonation-container">
+          <div className="header-container">
+            <h1>Clothes</h1>
+          </div>
+          <div className="rest-container" style={{ top: "16em" }}>
+            <h4>What size of clothing are you looking for?</h4>
+            <form className="radioButtons-form" method="get">
+              <RadioButton text={"Small"} />
+              <RadioButton text={"Medium"} />
+              <RadioButton text={"Large"} />
+            </form>
+            <p />
+            <h4> How many pieces do you need?</h4>
+            <NumberInputBox label={"Quantity"} width={"280px"} />
+          </div>
+        </div>
+      }
+      rightPanelButtonText={"Post"}
+      hasButton={true}
+    />
   );
 }
 
