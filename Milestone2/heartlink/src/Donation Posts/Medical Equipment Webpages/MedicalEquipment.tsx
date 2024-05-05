4355a2790082c792aea14c2253007ec57b0684cc
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-05 05:05:29 +0300

    added the choice of others to medical Equipment

--- Milestone2/heartlink/src/Donation Posts/Medical Equipment Webpages/MedicalEquipment.tsx
+++ Milestone2/heartlink/src/Donation Posts/Medical Equipment Webpages/MedicalEquipment.tsx
@@ -2,8 +2,14 @@ import "../createPost/createPost.css";
 import Template1 from "../../components/Templates/Template1.tsx";
 import MedicalSupplies from "../../components/MedicalSupplies/MedicalSupplies.tsx";
 import DropDown from "../../components/DropDown/DropDown.tsx";
+import { useState } from "react";
 
 function CreatePost() {
+  const [disabled, setDisabled] = useState(true);
+
+  function handleEquipChange(equip: string) {
+    setDisabled(!(equip === "Others"));
+  }
   return (
     <Template1
       leftPanelDiv={
@@ -13,7 +19,11 @@ function CreatePost() {
         </div>
       }
       rightPanelDiv={
-        <MedicalSupplies name={"equipment"} header={"Medical Equipment"}>
+        <MedicalSupplies
+          name={"equipment"}
+          header={"Medical Equipment"}
+          disabled={disabled}
+        >
           <DropDown
             options={[
               "Diagnostic Equipment",
@@ -22,13 +32,15 @@ function CreatePost() {
               "Therapeutic Equipment",
               "Surgical Equipment",
               "Rehabilitation Equipment",
+              "Others",
             ]}
             selected={"Specify equipment category"}
             width={"280px"}
+            onChange={handleEquipChange}
           />
         </MedicalSupplies>
       }
-      rightPanelButtonText={"Post"}
+      rightPanelButtonText={"Next"}
       hasButton={true}
     />
   );
