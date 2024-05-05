f46ae2247afc357b39b00fb3aa3fb7cffbd76054
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-05 05:02:17 +0300

    finished food donation webpage

--- Milestone2/heartlink/src/Donation Posts/Food Donation Webpages/Food.tsx
+++ Milestone2/heartlink/src/Donation Posts/Food Donation Webpages/Food.tsx
@@ -2,6 +2,7 @@ import NumberInputBox from "../../components/InputBox/NumberInputBox.tsx";
 import DropDown from "../../components/DropDown/DropDown.tsx";
 import { useState } from "react";
 import hashSet from "../../hashSet";
+import Template1 from "../../components/Templates/Template1.tsx";
 
 function Food() {
   const foodOptions: hashSet = {
@@ -35,44 +36,60 @@ function Food() {
   };
 
   return (
-    <div>
-      <div className="header-container">
-        <h1>Food</h1>
-      </div>
-      <div className="rest-container" style={{ top: "10em" }}>
-        <h4> What category of food do you need?</h4>
-        <DropDown
-          options={Object.keys(foodOptions)}
-          selected={"Specify food category"}
-          width={"280px"}
-          onChange={handleCategoryChange}
-        />
-        <br />
-        <br />
-        <h4>What specific food do you need?</h4>
-        <h6> (Please select a category first)</h6>
-        <DropDown
-          options={selectedCategory ? foodOptions[selectedCategory] : []}
-          selected={selectedFood || "Select food"}
-          width={"280px"}
-          onChange={(food) => setSelectedFood(food)}
-          disabled={!selectedCategory}
-        />
-        <br />
-        <br />
-        {selectedCategory === "Fruits" || selectedCategory === "Vegetables" ? (
-          <>
-            <h4> How much {selectedCategory.toLowerCase()} do you need?</h4>
-            <NumberInputBox label={"Quantity (in KG)"} width={"280px"} />{" "}
-          </>
-        ) : (
-          <>
-            <h4> How much food do you need?</h4>
-            <NumberInputBox label={"Quantity"} width={"280px"} />{" "}
-          </>
-        )}
-      </div>
-    </div>
+    <Template1
+      leftPanelDiv={
+        <div className="leftText">
+          <p /> 1 of 1<h2> Creating your post!</h2>
+          <p> Almost Done.. </p>
+        </div>
+      }
+      rightPanelDiv={
+        <div>
+          <div className="header-container">
+            <h1>Food</h1>
+          </div>
+          <div className="rest-container" style={{ top: "10em" }}>
+            <h4> What category of food do you need?</h4>
+            <DropDown
+              options={Object.keys(foodOptions)}
+              selected={"Specify food category"}
+              width={"280px"}
+              onChange={handleCategoryChange}
+            />
+            <br />
+            <br />
+            <h4>What specific food do you need?</h4>
+            <h6> (Please select a category first)</h6>
+            <DropDown
+              options={selectedCategory ? foodOptions[selectedCategory] : []}
+              selected={selectedFood || "Select food"}
+              width={"280px"}
+              onChange={(food) => setSelectedFood(food)}
+              disabled={!selectedCategory}
+            />
+            <br />
+            <br />
+            {selectedCategory === "Fruits" ||
+            selectedCategory === "Vegetables" ? (
+              <>
+                <h4> How much {selectedCategory.toLowerCase()} do you need?</h4>
+                <NumberInputBox
+                  label={"Quantity (in KG)"}
+                  width={"280px"}
+                />{" "}
+              </>
+            ) : (
+              <>
+                <h4> How much food do you need?</h4>
+                <NumberInputBox label={"Quantity"} width={"280px"} />{" "}
+              </>
+            )}
+          </div>
+        </div>
+      }
+      rightPanelButtonText={"Post"}
+      hasButton={true}
+    />
   );
 }
 
