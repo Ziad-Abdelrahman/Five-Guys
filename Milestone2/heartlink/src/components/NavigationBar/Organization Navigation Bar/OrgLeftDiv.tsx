f1318fbe6de5c4b994ef1709489f923ba8acec53
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-06 15:52:57 +0300

    added org nav bar

--- Milestone2/heartlink/src/components/NavigationBar/Organization Navigation Bar/OrgLeftDiv.tsx
+++ Milestone2/heartlink/src/components/NavigationBar/Organization Navigation Bar/OrgLeftDiv.tsx
@@ -1,31 +1,89 @@
-import './OrgLeftDiv.css'
-import '../../Buttons/Buttons.css'
+import "./OrgLeftDiv.css";
+import "../Donor Navigation Bar/DonorLeftDiv.css";
+import "../../Buttons/Buttons.css";
 import arrow from "../../../assets/right-arrow.png";
-function OrgLeftDiv(){
+import React, { useState } from "react";
+import minilogo from "../../../assets/minilogo.png";
+import { IoClose } from "react-icons/io5";
+import DropDown from "../../DropDown/DropDown";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-    return(
-        <>
-            <div className="dropdown">
-                <button className="dropbtn">Posts
-                    <img className="arrow-in-button" src={arrow} alt="arrow"/>
-                </button>
-                <div className="dropdown-content">
-                <a href="#">View All Posts</a>
-                    <a href="#">Create Post</a>
-                    <a href="#">Delete Post</a>
-                    <a href="#">Edit Post</a>
-                </div>
+function OrgLeftDiv() {
+  const [showSubmit, setShowSubmit] = useState(false);
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedTiming, setSelectedTiming] = useState("");
+  const [pendingTiming, setPendingTiming] = useState("");
+
+  const handleCloseEvent = () => {
+    setShowSubmit(false);
+    setConfirmed(false);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setConfirmed(true);
+    setSelectedTiming(pendingTiming);
+  };
+  const handleTimingSelect = (timing: string) => {
+    setPendingTiming(timing);
+  };
+
+  return (
+    <>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Posts
+          <img className="arrow-in-button" src={arrow} alt="arrow" />
+        </button>
+        <div className="dropdown-content">
+          <Link to="ViewPosts">View All Posts</Link>
+          <Link to="CreateAPost">Create Post</Link>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Drop-off Time
+          <img className="arrow-in-button" src={arrow} alt="arrow" />
+        </button>
+        <div className="dropdown-content">
+          <a onClick={() => setShowSubmit(true)}>Specify Drop-off timing </a>
+        </div>
+      </div>
+      {showSubmit && (
+        <div className="Submit">
+          <div className="topOfSubmit">
+            <img src={minilogo} />
+            <IoClose className="xSubmit" onClick={() => handleCloseEvent()} />
+          </div>
+          <h4 className={"specify-timing"}>Specify Preferred Drop-off Time</h4>
+          <form onSubmit={handleSubmit}>
+            <DropDown
+              options={[
+                "10:00 AM- 12:00 PM",
+                "12:00 PM - 2:00 PM",
+                "2:00 PM - 4:00 PM",
+                "4:00 PM - 6:00 PM",
+              ]}
+              selected={"From .. To .. "}
+              onChange={handleTimingSelect}
+              width={"280px"}
+            />
+            <button type="submit" className="confirm-button">
+              Confirm
+            </button>
+          </form>
+          {confirmed && (
+            <div className="confirmation-message">
+              <div className="confirmation-div">
+                <IoCheckmarkCircleOutline className="confirmation-icon" />
+                <p> Delivery Confirmed: {selectedTiming} </p>
+              </div>
             </div>
-            <button className="dropbtn">Drop-off Time
-                    <img className="arrow-in-button" src={arrow} alt="arrow"/>
-                </button>
-
-
-
-
-        </>
-    );
-
+          )}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default OrgLeftDiv;
--- Milestone2/heartlink/src/components/NavigationBar/Organization Navigation Bar/OrgRightDiv.tsx
+++ Milestone2/heartlink/src/components/NavigationBar/Organization Navigation Bar/OrgRightDiv.tsx
@@ -1,36 +1,123 @@
-import Profilecircle from "../Donor Navigation Bar/profileCircle";
+import Profilecircle from "./Profilecircle";
 import { IoNotificationsOutline } from "react-icons/io5";
+import { TbTruckDelivery } from "react-icons/tb";
+import { useState } from "react";
+import { BsEnvelopeOpenHeart } from "react-icons/bs";
+
+import "./OrgRightDiv.css";
+
 function OrgRightDiv() {
+  // State to manage the visibility of the notifications dropdown
+  const [showNotifications, setNotifications] = useState(false);
+
+  // Function to toggle the visibility of the dropdown
+  const toggleDropdown = () => {
+    setNotifications((prevState) => !prevState); // Correct toggle function
+  };
   return (
     <>
-      <IoNotificationsOutline
-        style={{ fontSize: "28px", marginRight: "40px" }}
-        className="notification-icon"
-      />
-      <div className="profiledropdown">
-        {/* Hidden checkbox */}
-        <input
-          type="checkbox"
-          id="dropdownCheckbox"
-          className="dropdownCheckbox"
-          style={{ display: "none" }}
-        />
-        {/* Label for the checkbox, which will be the clickable area */}
-        <label htmlFor="dropdownCheckbox" className="profileCircleContainer">
-          <Profilecircle />
-        </label>
-        {/* Dropdown content that shows up when checkbox is checked */}
-        <ul className="profiledropdownContent">
-          <li>
-            <a href="#">Account info</a>
-          </li>
-          <li>
-            <a href="#">Settings</a>
-          </li>
-          <li>
-            <a href="#">Logout</a>
-          </li>
-        </ul>
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <div>
+          <IoNotificationsOutline
+            className="notification-icon"
+            style={{ fontSize: "28px", marginRight: "40px", cursor: "pointer" }}
+            onClick={toggleDropdown}
+          />
+          {showNotifications && (
+            <div className="notifications-dropdown">
+              <div className="notification-container">
+                <div style={{ display: "flex", alignItems: "center", flex: 1 }}>
+                  <BsEnvelopeOpenHeart className={"envelope-icon"} />
+                  <p>Toys Donation fulfilled!</p>
+                </div>
+                <div
+                  style={{
+                    fontSize: "12px",
+                    textAlign: "right",
+                    marginTop: "5px",
+                  }}
+                >
+                  Just Now
+                </div>
+              </div>
+              <div className="notification-container">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flex: 1,
+                    marginTop: "20px",
+                  }}
+                >
+                  <TbTruckDelivery style={{ fontSize: "30px" }} />
+                  <p>Your Driver arrived!</p>
+                </div>
+                <div
+                  style={{
+                    fontSize: "12px",
+                    textAlign: "right",
+                    marginTop: "30px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  10 min ago
+                </div>
+              </div>
+              <div className="notification-container">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flex: 1,
+                    marginTop: "10px",
+                  }}
+                >
+                  <BsEnvelopeOpenHeart className={"envelope-icon"} />
+                  <p>Blood Donation fulfilled!</p>
+                </div>
+                <div
+                  style={{
+                    fontSize: "12px",
+                    textAlign: "right",
+                    marginTop: "10px",
+                  }}
+                >
+                  5 min ago
+                </div>
+              </div>
+              <hr />
+            </div>
+          )}
+        </div>
+
+        {/*profile picture drop down*/}
+        <div className="profiledropdown">
+          {/* Hidden checkbox */}
+          <input
+            type="checkbox"
+            id="dropdownCheckbox"
+            className="dropdownCheckbox"
+            style={{ display: "none" }}
+          />
+          {/* Label for the checkbox, which will be the clickable area */}
+          <label htmlFor="dropdownCheckbox" className="profileCircleContainer">
+            <Profilecircle />
+          </label>
+          {/* Dropdown content that shows up when checkbox is checked */}
+          <ul className="profiledropdownContent">
+            <li>
+              <a href="#">Account info</a>
+            </li>
+            <li>
+              <a href="#">Settings</a>
+            </li>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
