bf6bf2e3e33fbd9becbbe0e916aa229aedaf8521
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-08 15:28:33 +0300

    added admin navbar

---
+++ Milestone2/heartlink/src/components/NavigationBar/AdminNavbar/AdminRightDiv.tsx
@@ -0,0 +1,43 @@
+
+import Profilecircle from "./Profilecircle";
+
+
+function AdminRightDiv() {
+
+
+  return (
+    <>
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+       
+
+        {/*profile picture drop down*/}
+        <div className="profiledropdown">
+          {/* Hidden checkbox */}
+          <input
+            type="checkbox"
+            id="dropdownCheckbox"
+            className="dropdownCheckbox"
+            style={{ display: "none" }}
+          />
+          {/* Label for the checkbox, which will be the clickable area */}
+          <label htmlFor="dropdownCheckbox" className="profileCircleContainer">
+            <Profilecircle />
+          </label>
+          {/* Dropdown content that shows up when checkbox is checked */}
+          <ul className="profiledropdownContent">
+            <li>
+              <a href="#">Change Password</a>
+            </li>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AdminRightDiv;
