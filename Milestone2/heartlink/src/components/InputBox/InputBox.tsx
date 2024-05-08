c52e7e161a497d8e7b9ff29988f3dd429f8eccd4
m-halawa <menna.halawa@gmail.com>
2024-05-08 20:25:59 +0300

    blood donation show error message

--- Milestone2/heartlink/src/components/InputBox/InputBox.tsx
+++ Milestone2/heartlink/src/components/InputBox/InputBox.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import "./InputBox.css"; // Import your CSS file for styling
 
 interface InputBoxProps {
@@ -5,18 +6,27 @@ interface InputBoxProps {
   label: string;
   width: string;
   disabled?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function InputBox({ type, label, width, disabled = false }: InputBoxProps) {
+function InputBox({ type, label, width, disabled = false, onChange }: InputBoxProps) {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(e); // Call the onChange handler passed from parent component
+    }
+  };
+
   return (
-    <div className="form-floating mb-3" style={{ width: width }}>
+    <div className="form-floating mb-3" style={{ width: width }} >
       <input
         type={type}
         className="form-control"
         id="floatingInput"
         disabled={disabled}
+        onChange={handleChange}
       />
-      <label>{label}</label>
+      <label htmlFor="floatingInput">{label}</label>
     </div>
   );
 }
