9f7ddb5ecefe1df0b0dc93fb9bcdca8b149f0003
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-05-08 03:56:32 +0300

    feat(AccountManagement): add on click view info card

--- Milestone2/heartlink/src/Donor Account Management/donorAccountMangPage.tsx
+++ Milestone2/heartlink/src/Donor Account Management/donorAccountMangPage.tsx
@@ -2,11 +2,28 @@ import DonorNavigationBar from "../components/NavigationBar/Donor Navigation Bar
 import HeaderOfSection from "../components/Header/HeaderOfSection.tsx";
 import DonorAccountProfileCard from "../components/Card/DonorAccountProfileCard.tsx";
 import DonorProbonoCard from "../components/Card/DonorProbonoCard.tsx";
+
 import "./donorAccountMangPage.css";
 
 import Doctor from "../assets/doctor.png";
 import Teacher from "../assets/teachervector.png";
+import { useState } from "react";
+
+import DonorAddTeacherInfo from "./donorAddTeacherInfo.tsx";
+import DonorAddDoctorInfo from "./donorAddDoctorInfo.tsx"; // Ensure this component exists and is imported correctly
+
 function donorAccountMangPage() {
+  const [selectedProbono, setSelectedProbono] = useState(null);
+
+  // Function to toggle the visibility of the dropdown based on the card clicked
+  const toggleProbonoInfo = (probonoType) => {
+    if (selectedProbono === probonoType) {
+      setSelectedProbono(null); // If the same type is clicked again, it will hide
+    } else {
+      setSelectedProbono(probonoType); // Set to the selected type
+    }
+  };
+
   return (
     <>
       <DonorNavigationBar />
@@ -14,15 +31,27 @@ function donorAccountMangPage() {
       <div className="management-cards-container">
         <DonorAccountProfileCard />
         <div className={"master-probono-container"}>
-          <div id={"card-and-drop-down-container"} style={{ marginLeft: "4%" }}>
-            <DonorProbonoCard image={Doctor} title={"Become a Doctor"} />
+          <div className={"pro-bono-cards-container"}>
+            <div
+              className={"card-and-drop-down-container"}
+              onClick={() => toggleProbonoInfo("doctor")}
+            >
+              <DonorProbonoCard image={Doctor} title={"Become a Doctor"} />
+            </div>
+
+            <div
+              className={"card-and-drop-down-container"}
+              onClick={() => toggleProbonoInfo("teacher")}
+            >
+              <DonorProbonoCard image={Teacher} title={"Become a Teacher"} />
+            </div>
           </div>
-          <div id={"card-and-drop-down-container"} style={{ marginLeft: "7%" }}>
-            <DonorProbonoCard image={Teacher} title={"Become a Teacher"} />
-          </div>
+          {selectedProbono === "teacher" && <DonorAddTeacherInfo />}
+          {selectedProbono === "doctor" && <DonorAddDoctorInfo />}
         </div>
       </div>
     </>
   );
 }
+
 export default donorAccountMangPage;
