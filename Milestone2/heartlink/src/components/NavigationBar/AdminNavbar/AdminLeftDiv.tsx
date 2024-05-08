8d30b9651f1d215470d6178cadeecad2e5a72ace
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-08 15:28:24 +0300

    added admin navbar

---
+++ Milestone2/heartlink/src/components/NavigationBar/AdminNavbar/AdminLeftDiv.tsx
@@ -0,0 +1,31 @@
+import "../../Buttons/Buttons.css";
+import arrow from "../../../assets/right-arrow.png";
+import "./AdminLeftDiv.css";
+function AdminLeftDiv() {
+  return (
+    <>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Organizations
+          <img className="arrow-in-button" src={arrow} alt="arrow" />
+        </button>
+        <div className="dropdown-content">
+          <a href="#">View Registered Organizations</a>
+          <a href="#">Accept/Reject Submissions</a>
+        </div>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Donors
+          <img className="arrow-in-button" src={arrow} alt="arrow" />
+        </button>
+        <div className="dropdown-content">
+          <a href="#">View Donors List</a>
+          <a href="#">Accept/Reject Requests</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AdminLeftDiv;
