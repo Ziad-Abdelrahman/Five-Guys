7b93b1408ad372bf6337eac28a006a2b12e66955
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-09 03:09:58 +0300

    adding error messages

--- Milestone2/heartlink/src/components/InputBox/NumberInputBox.tsx
+++ Milestone2/heartlink/src/components/InputBox/NumberInputBox.tsx
@@ -3,10 +3,10 @@ import "./InputBox.css"; // Import your CSS file for styling
 interface InputBoxProps {
   label: string;
   width: string;
+  handleChange?: (quantity: string) => void;
 }
 
-function NumberInputBox({ label, width }: InputBoxProps) {
-  
+function NumberInputBox({ label, width, handleChange }: InputBoxProps) {
   return (
     <div className="form-floating mb-3" style={{ width: width }}>
       <input
@@ -15,7 +15,9 @@ function NumberInputBox({ label, width }: InputBoxProps) {
         id="floatingInput"
         placeholder="name@example.com"
         min="0"
-
+        onClick={(e) => {
+          handleChange && handleChange(e.currentTarget.value);
+        }}
       />
       <label>{label}</label>
     </div>
--- Milestone2/heartlink/src/components/Templates/RightPanel.tsx
+++ Milestone2/heartlink/src/components/Templates/RightPanel.tsx
@@ -2,14 +2,15 @@ import BackButton from "../Buttons/BackButton.tsx";
 import Button from "../Buttons/Button.tsx";
 import "./RightPanel.css";
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface RightPanelProps {
   text: string;
   backwardPath?: string;
   children: ReactNode;
+  forwardPath: string;
   hasBackButton?: boolean;
-  handleClick?: () => void;
+  handleClick?: () => boolean;
 }
 
 function RightPanel({
@@ -17,17 +18,25 @@ function RightPanel({
   text,
   hasBackButton,
   backwardPath,
+  forwardPath,
   handleClick,
 }: RightPanelProps) {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    if (handleClick && !handleClick()) return;
+    let path = forwardPath;
+    navigate(path);
+  };
+
   return (
     <div className="rightPanel-container">
       {children}
       <div className="footer">
         <div className="footerButtons-container">
-          <Button text={text} handleClick={handleClick} />
+          <Button text={text} handleClick={routeChange} />
           {hasBackButton && backwardPath && (
             <Link to={backwardPath}>
-              <BackButton />{" "}
+              <BackButton />
             </Link>
           )}
         </div>
