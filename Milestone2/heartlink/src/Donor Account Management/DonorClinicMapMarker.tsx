1a23db9920c2f4889fd8252f174d848920489c70
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-05-12 04:47:56 +0300

    feat(AccountManag): add doctor info step3

---
+++ Milestone2/heartlink/src/Donor Account Management/DonorClinicMapMarker.tsx
@@ -0,0 +1,89 @@
+import InputBox from "../components/InputBox/InputBox.tsx";
+import DonateButton from "../components/Buttons/DonateButton.tsx";
+import "./donorAccountMangPage.css";
+import Map from "../components/map/Map.tsx";
+import { useState } from "react";
+function DonorClinicAddress() {
+  const [address, setAddress] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isUnderReview, setUnderReview] = useState(false);
+
+  function handleFinishClick() {
+    if (!address.trim()) {
+      setErrorMessage("Please fill in the Address field.");
+    } else {
+      setErrorMessage("");
+      setUnderReview(true);
+    }
+  }
+
+  return (
+    <>
+      {/*main container for probono info*/}
+      <div className="probono-info-container">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "70%",
+          }}
+        >
+          <h2>Clinic Location</h2>
+          <div style={{ padding: "0 4%" }}>
+            <div
+              style={{
+                marginTop: "10%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <h5 style={{ marginRight: "10%", width: "41%" }}>
+                Enter Address
+              </h5>
+              <InputBox
+                width={"99%"}
+                label={"Enter Full Address"}
+                type={"string"}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div
+              style={{
+                marginTop: "10%",
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div onClick={handleFinishClick}>
+                <DonateButton text={"Finish"} />
+              </div>
+              {errorMessage && (
+                <div
+                  style={{
+                    display: "flex",
+                    color: "red",
+                    marginLeft: "10px",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  {errorMessage}
+                </div>
+              )}
+              {isUnderReview && (
+                <h3 style={{ marginLeft: "10px", color: "#01A95D" }}>
+                  Your Data Are Under Review!
+                </h3>
+              )}
+            </div>
+          </div>
+        </div>
+        <Map height={"270px"} width={"250px"} />
+      </div>
+    </>
+  );
+}
+
+export default DonorClinicAddress;
